29d855f71997b54892fc5247904011e91bffe6f3
merhawi <merhawi.temnewo88@gmail.com>
2019-03-26 20:34:42 -0500

    added streams

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 class App extends Component {
-render() {
-    return <div>App</div>
-}
-
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/show" exact component={StreamShow} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
