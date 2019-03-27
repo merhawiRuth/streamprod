098d641ae5da5d22411b028aa14c1a8515124b0e
merhawi <merhawi.temnewo88@gmail.com>
2019-03-26 21:03:54 -0500

    finished navigation course

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
+import Header from './header';
 class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
           <div>
+            <Header />
+
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
